c0f66e6cc7f47a1392c528a8c7ae63b5e4737ccf
lisongyu <269876940@qq.com>
2021-08-01 10:45:16 +0800

    提交插件文件上传

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,16 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path =require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { DefinePlugin } = require('webpack')
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-
+const AutoUploadPlugin=require('./plugins/AutoUploadPlugin')
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/main.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
-  module: {
-    rules: [
-      {
-        test: /\.png/,
-        type: 'asset/resource',
-        generator: {
-          filename: "img/[name].[hash:6][ext]"
-        }
-      }
-    ]
-  },
   plugins: [
-    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "coderwhy webpack",
-      template: './public/index.html'
+      template:'./src/index.html'
     }),
-    new DefinePlugin({
-      BASE_URL: '"./"'
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: "public",
-          globOptions: {
-            ignore: [
-              "**/index.html",
-              "**/.DS_Store"
-            ]
-          }
-        },
-      ],
-    }),
+    new AutoUploadPlugin()
   ]
 };
